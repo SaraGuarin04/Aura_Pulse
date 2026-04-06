7b1f4214012680bb80700dd9eb5676eca4488ba3
SaraGuarin04 <sarac.guarin@gmail.com>
2026-04-06 10:24:22 -0500

    Update index.ts

--- src/api/v2/index.ts
+++ src/api/v2/index.ts
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import UserRouter from "../../modules/users/users_routes";
 import AuthRouter from "../../modules/auth/auth_routes";
-import ActionRouter from "../../modules/eco_actions/action_routers";
+import ActionRouter from "../../modules/action/action_routers";
 
 const router = Router();
 
