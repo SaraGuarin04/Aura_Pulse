15b011f3c2ad61104cf1cf4b96f785b57f58bc1b
SaraGuarin04 <sarac.guarin@gmail.com>
2026-04-06 10:21:03 -0500

    Create index.ts

---
+++ src/api/v2/index.ts
@@ -0,0 +1,13 @@
+import { Router } from "express";
+import UserRouter from "../../modules/users/users_routes";
+import AuthRouter from "../../modules/auth/auth_routes";
+import ActionRouter from "../../modules/eco_actions/action_routers";
+
+const router = Router();
+
+
+router.use('/auth', AuthRouter);
+router.use('/users', UserRouter);
+router.use('/action', ActionRouter);
+
+export default router;
