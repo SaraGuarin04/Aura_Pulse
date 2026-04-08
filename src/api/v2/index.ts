366e2decf58a59f3eeb1029dca75d979237a3270
SaraGuarin04 <sarac.guarin@gmail.com>
2026-04-08 16:00:42 -0500

    Update index.ts

--- src/api/v2/index.ts
+++ src/api/v2/index.ts
@@ -2,12 +2,14 @@ import { Router } from "express";
 import UserRouter from "../../modules/users/users_routes";
 import AuthRouter from "../../modules/auth/auth_routes";
 import ActionRouter from "../../modules/action/action_routers";
-
+import RankingRouter from "../../modules/ranking/ranking_routes";
 const router = Router();
 
 
 router.use('/auth', AuthRouter);
 router.use('/users', UserRouter);
 router.use('/action', ActionRouter);
+router.use('/ranking', RankingRouter);
+
 
 export default router;
