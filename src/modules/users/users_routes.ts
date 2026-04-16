2b5178a3919ae0a201a62aed0c7ebcc8b0d8e8ce
SaraGuarin04 <sarac.guarin@gmail.com>
2026-04-15 19:25:48 -0500

    Update users_routes.ts

--- src/modules/users/users_routes.ts
+++ src/modules/users/users_routes.ts
@@ -2,12 +2,12 @@ import { Router } from "express";
 import { UsersController } from "./users_controllers";
 import { createUserSchema } from "./users_schema";
 import { validate } from "../../middlewares/validate.middleware";
-
+import { authMiddleware } from "../../middlewares/auth.middleware";
 
 const router = Router();
 const _UsersController = new UsersController();
 
 router.post('/register', validate(createUserSchema), _UsersController.register);
-router.get('/', _UsersController.findAllUsers);
+router.get('/', authMiddleware,_UsersController.findAllUsers);
 
 export default router;
