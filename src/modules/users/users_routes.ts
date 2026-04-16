5d810a22e31a2fde065a34b092c2cfe86c64aacc
SaraGuarin04 <sarac.guarin@gmail.com>
2026-04-15 19:33:19 -0500

    Update users_routes.ts

--- src/modules/users/users_routes.ts
+++ src/modules/users/users_routes.ts
@@ -2,7 +2,7 @@ import { Router } from "express";
 import { UsersController } from "./users_controllers";
 import { createUserSchema } from "./users_schema";
 import { validate } from "../../middlewares/validate.middleware";
-import { authMiddleware } from "../../middlewares/auth.middleware";
+import { authMiddleware } from "../../middlewares/auth.middlewares";
 
 const router = Router();
 const _UsersController = new UsersController();
