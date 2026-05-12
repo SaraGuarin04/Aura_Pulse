32c61778e533d52948f5292001e2411159589970
SaraGuarin04 <sarac.guarin@gmail.com>
2026-05-12 16:31:55 -0500

    Update index.ts

--- src/api/v2/index.ts
+++ src/api/v2/index.ts
@@ -2,14 +2,15 @@ import { Router } from "express";
 import UserRouter from "../../modules/users/users_routes";
 import AuthRouter from "../../modules/auth/auth_routes";
 import ActionRouter from "../../modules/action/action_routers";
-import RankingRouter from "../../modules/ranking/ranking_routes";
-const router = Router();
+import challengeRoutes from '../../modules/challenge/challenge_routes';
+import achievementRoutes from '../../modules/Achievements/achievements_routes';
 
+const router = Router();
 
 router.use('/auth', AuthRouter);
 router.use('/users', UserRouter);
 router.use('/action', ActionRouter);
-router.use('/ranking', RankingRouter);
-
+router.use('/challenges', challengeRoutes);
+router.use('/achievements', achievementRoutes);
 
 export default router;
